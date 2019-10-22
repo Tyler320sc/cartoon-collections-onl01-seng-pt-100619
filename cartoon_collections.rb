989d6d17bce5c9ddeedb7b3cb5b92f6074a2c721
Tyler Carroll <tylercarroll1611@gmail.com>
2019-10-22 00:38:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+def roll_call_dwarves(dwarf_names)
+  dwarf_names.each_with_index do |names, index|
+    puts "#{index+1} #{names}"
+  end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map do |element|
+    # binding.pry 
+    "#{element.capitalize}!"
+#     element.capitalize
+
+  end 
 end
+
+# names.map! {|name| name.capitalize } # now names contains ['Danil', 'Edmund']
 
 def long_planeteer_calls# code an argument here
   # Your code here
